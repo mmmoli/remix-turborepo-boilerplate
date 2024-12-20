31e1ef58fe804430c177913db9af6f1f5a6491d9
Michele Memoli <michele.memoli@gmail.com>
2024-12-20 22:00:26 +0700

    fix(shared-ui): updated tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,8 +1,11 @@
-import baseConfig from '@repo/ui/tailwind.config'
-import type { Config } from 'tailwindcss'
+import baseConfig from "@repo/ui/tailwind.config";
+import type { Config } from "tailwindcss";
 
 export default {
-	content: ['./app/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
-	presets: [baseConfig],
-	theme: {},
-} satisfies Config
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "../../packages/shared-ui/src/**/*.{ts,tsx}",
+  ],
+  presets: [baseConfig],
+  theme: {},
+} satisfies Config;
